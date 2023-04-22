d00386886c0574b0640b77507f472767c1bcbbe2
Felipe Rangel <78965577+felipesrangel@users.noreply.github.com>
2023-04-22 16:55:32 -0300

    criando rotas filhas

--- src/app/pages/sobre/sobre.component.ts
+++ src/app/pages/sobre/sobre.component.ts
@@ -1,10 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { HomeComponent } from '../home/home.component';
 
 @Component({
   selector: 'app-sobre',
   templateUrl: './sobre.component.html',
   styleUrls: ['./sobre.component.scss']
 })
-export class SobreComponent {
+export class SobreComponent  implements OnInit{
 
+  constructor(
+    private activedRoute: ActivatedRoute,
+    private router: Router,
+    ){}
+
+
+  ngOnInit(): void {
+      this.activedRoute.params.subscribe(
+        res => console.log(res['id'], res['username'], res)
+
+      )
+
+      this.activedRoute.queryParams.subscribe(
+        res => console.log(res)
+      )
+
+      setInterval( () => {
+        this.router.navigate(['404']);
+        //this.router.navigateByUrl('404');
+      }, 5000)
+  }
 }
